62147bec761b3a67f94c5b2009f582d845732e2e
janvisharma <janvi@connect.hku.hk>
2020-07-23 01:30:07 +0530

    Updates

---
+++ precache-manifest.d4799a2a44f058d70abd90bca030a7fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211b2a0e43dcaad8aed8ad0340f68f59",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "93668eb377dbb8d21868",
+    "url": "/To-Do-List/static/css/2.58c4053f.chunk.css"
+  },
+  {
+    "revision": "14a3f0687dd5f2b1c71b",
+    "url": "/To-Do-List/static/css/main.35f67bf4.chunk.css"
+  },
+  {
+    "revision": "93668eb377dbb8d21868",
+    "url": "/To-Do-List/static/js/2.001fd0ab.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/To-Do-List/static/js/2.001fd0ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a3f0687dd5f2b1c71b",
+    "url": "/To-Do-List/static/js/main.09587db7.chunk.js"
+  },
+  {
+    "revision": "3fc2ca48924706e73961",
+    "url": "/To-Do-List/static/js/runtime-main.ce724f51.js"
+  },
+  {
+    "revision": "6140613f39eab6af938437a06dde0d7f",
+    "url": "/To-Do-List/static/media/bg.6140613f.jpg"
+  }
+]);
